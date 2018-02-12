c8e97760dee72e42080939f1c2549bfd3953d0ef
ITStep Java <itstep.java@gmail.com>
2018-02-12 21:35:31 +0200

    Added outline for the user list screen.

---
+++ app/src/main/java/bg/itstep/chatclient/UserListActivity.java
@@ -0,0 +1,45 @@
+package bg.itstep.chatclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ITstep on 2/12/2018.
+ */
+
+public class UserListActivity extends Activity {
+
+    private ArrayList<String> users = new ArrayList<>();
+    private ArrayAdapter usersAdapter;
+
+    private ListView usersList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set the user interface of this activity to the specified XML file.
+        setContentView(R.layout.activity_user_list);
+
+        usersList = findViewById(R.id.users_list);
+        usersAdapter =
+                new ArrayAdapter(this, android.R.layout.simple_list_item_1, users);
+
+        usersList.setAdapter(usersAdapter);
+
+        updateUsersList();
+    }
+
+    private void updateUsersList() {
+        users.add("USER1");
+        users.add("USER2");
+        users.add("USER3");
+        users.add("USER4");
+
+        usersAdapter.notifyDataSetChanged();
+    }
+}
